b675d7b1ea742d7129dc9eaf505a2a23d22c45be
Mikhail Levin <mikhaillevin@inbox.ru>
2021-11-28 00:48:19 +0300

    Updated Purchase structure

--- backend/internal/models/shopping_list.go
+++ backend/internal/models/shopping_list.go
@@ -3,5 +3,6 @@ package models
 type Purchase struct {
 	Id          string `json:"purchase_id"`
 	Item        string `json:"name"`
+	Multiplier  int    `json:"multiplier"`
 	IsPurchased bool   `json:"is_purchased"`
 }
